7e910233c27f97da57d64252208c4addd0cb85e9
coastaldemigod <shivanshagrawal2411@gmail.com>
2020-07-22 18:18:07 +0530

    navbar fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,15 @@
+var checkScrollBar = function(){
+  $('nav').css({
+    backgroundColor: $(this).scrollTop() > 1 ?
+      'rgb(255, 255, 255) ' : 'transparent'
+  })
+};
+
 $(document).ready(function(){
 
-    var checkScrollBar = function(){
-      $('nav').css({
-        backgroundColor: $(this).scrollTop() > 1 ?
-          'rgb(255, 255, 255) ' : 'transparent'
-      })
-    }
-    $(document).ready(function(){
-      $("nav").hover(function(){
-        $(this).css("background-color", "#fff");
-        }, function(){
-        $(this).css("background-color", "transparent");
-      });
+
+  $(window).on('load resize scroll', checkScrollBar)
+   
     });
 
-      $(window).on('load resize scroll', checkScrollBar)
-    });
     
