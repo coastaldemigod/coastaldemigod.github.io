d1cc0f3a5fec93403c362c84a929ac845e811646
coastaldemigod <shivanshagrawal2411@gmail.com>
2020-10-04 18:48:50 +0530

    navbar shadow added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,17 @@
 var checkScrollBar = function(){
-  $('nav').css({
-    backgroundColor: $(this).scrollTop() > 1 ?
-      'rgb(255, 255, 255) ' : 'transparent'
-  })
+  if($(window).scrollTop()>1){
+    $('nav').addClass('navbar-animation');
+  }
+  else {
+    $('nav').removeClass('navbar-animation');
+  }
 };
-
+// This gives the navbar an animation
 $(document).ready(function(){
-
-
   $(window).on('load resize scroll', checkScrollBar)
-   
     });
 
+    // This collapses the navbar in responsive mode when clicked on one of the links
     $('.navbar-collapse a').click(function(){
       $(".navbar-collapse").collapse('hide');
   });
-
-    
