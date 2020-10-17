f8137caa1db2fad43b45490b64f31e0e1f66ca5e
coastaldemigod <shivanshagrawal2411@gmail.com>
2020-10-17 22:18:43 +0530

    matrix type random color scheme added

--- Page2/app.js
+++ Page2/app.js
@@ -1,5 +1,17 @@
-var colors ={
-    
+var colors =[
+  "#36ba01",
+  "#009a22",
+  "#00ff2b",
+  "#009a22",
+  "#36ba01",
+  "#00ff00",
+  "#00FF41"
+]
+
+for( var i=0;i<document.getElementsByTagName('li').length;i++)
+{
+  document.getElementsByTagName('li')[i].style.color=colors[Math.floor(Math.random()*5+1)];
+  console.log(Math.floor(Math.random()*4+1));
 }
 
 var input = document.getElementsByClassName("ipbox")[0];
@@ -18,4 +30,10 @@ function add(){
     ls.appendChild(an);
     document.getElementsByClassName("list-items")[0].appendChild(ls);
     document.getElementsByClassName("ipbox")[0].value='';
+    
+for( var i=0;i<document.getElementsByTagName('li').length;i++)
+{
+  document.getElementsByTagName('li')[i].style.color=colors[Math.floor(Math.random()*5+1)];
+  console.log(Math.floor(Math.random()*4+1));
+}
 }
